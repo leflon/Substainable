37e9f7f66874021e45f9f28485e6af6d3568dc18
hexadelusional <chamouxadele@gmail.com>
2025-04-23 13:07:47 +0200

    Add auth form component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to Sub'stainable</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation for svelte</p>
+<script>
+	import AuthForm from '../components/AuthForm.svelte';
+
+</script>
+<AuthForm></AuthForm>
