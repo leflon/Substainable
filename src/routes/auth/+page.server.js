16e62fa718e161322319fff1a48340f6919d0ce2
mattelothere <matteo.launay@gmail.com>
2025-04-24 19:07:56 +0200

    Remove console.log

--- src/routes/auth/+page.server.js
+++ src/routes/auth/+page.server.js
@@ -1,7 +1,6 @@
 export const load = ({url}) => {
 	const loginError = url.searchParams.get('login_error');
 	const registerError = url.searchParams.get('register_error');
-	console.log(loginError, registerError);
 	return {
 			loginError,
 			registerError
